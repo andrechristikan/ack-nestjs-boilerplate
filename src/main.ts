b0b26f2ea825546ffa992ba62914a9606ae73886
andrechristikan <andrechristikan@gmail.com>
2020-09-29 01:12:14 +0700

    persiapan auth 2

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors();
-    // app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(
         configService.getEnv('APP_PORT'),
--- src/middleware/logger/logger.constant.ts
+++ src/middleware/logger/logger.constant.ts
@@ -3,7 +3,7 @@ export const LoggerMaxFiles = '7d';
 export const LoggerName = 'system';
 
 export const HttpLoggerFormat =
-    ':remote-addr - :remote-user [:date[iso]] - HTTP/:http-version [:status] :res[content-length]/:response-time ms ":referrer" ":user-agent" ":method :url" - Request :req[header] Params[:req-params] Body[:req-body]  - Response :res[header] Body[:res-body]';
+    ':remote-addr -- :remote-user [:date[iso]] -- HTTP/:http-version [:status] :res[content-length]/:response-time ms ":referrer" ":user-agent" ":method :url" -- Request :req[header] Params[:req-params] Body[:req-body] -- Response :res[header] Body[:res-body]';
 export const HttpLoggerSize = '10M';
 export const HttpLoggerMaxSize = '20M';
 export const HttpLoggerName = 'http';
