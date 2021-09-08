f93435308254c3ede41628278d7efdacc22ee133
andrechristikan <andrechristikan@gmail.com>
2021-09-08 13:05:39 +0700

    add message

--- src/message/languages/en/auth.ts
+++ src/message/languages/en/auth.ts
@@ -1,5 +1,6 @@
 export default {
     login: 'Login Success.',
+    refresh: 'Refresh Token Success',
     error: {
         emailNotFound: 'Email not found in our system',
         passwordNotMatch: 'Your password not match'
