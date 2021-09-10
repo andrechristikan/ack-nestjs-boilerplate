425ba2f40e7811abbc2ddbaf456a4cebaa7ed57c
andrechristikan <andrechristikan@gmail.com>
2021-09-11 00:55:23 +0700

    add file helper

--- src/helper/helper.decorator.ts
+++ src/helper/helper.decorator.ts
@@ -1,4 +1,6 @@
-import { Inject } from '@nestjs/common';
+import { Inject, applyDecorators, UseInterceptors } from '@nestjs/common';
+import { IAuthApplyDecorator } from 'src/auth/auth.interface';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 export function Helper(): (
     target: Record<string, any>,
@@ -6,4 +8,8 @@ export function Helper(): (
     index?: number
 ) => void {
     return Inject(`HelperService`);
+}
+
+export function File(field: string): IAuthApplyDecorator {
+    return applyDecorators(UseInterceptors(FileInterceptor(field)));
 }
