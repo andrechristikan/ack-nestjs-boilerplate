a7a48d93d0a8a2cf56418c8f53adbb254fa66947
andrechristikan <andrechristikan@gmail.com>
2021-11-04 12:38:17 +0700

    remove error constant

--- src/error/error.constant.ts
+++ src/error/error.constant.ts
@@ -1,17 +1,4 @@
-import { HttpStatus } from '@nestjs/common';
-
 export enum ENUM_STATUS_CODE_ERROR {
     TEST_ERROR = 5991,
     UNKNOWN_ERROR = 5999
 }
-
-export const ERROR_MESSAGE = {
-    TEST_ERROR: {
-        httpCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        messagePath: 'app.error.default'
-    },
-    UNKNOWN_ERROR: {
-        httpCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        messagePath: 'http.server.internalServerError'
-    }
-};
