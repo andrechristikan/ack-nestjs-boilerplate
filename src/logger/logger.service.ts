4cf85077730f26afeebd171a4b53940fb73ab25a
andrechristikan <andrechristikan@gmail.com>
2022-01-01 17:00:52 +0700

    testing env, logger

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -4,13 +4,19 @@ import { Model, Types } from 'mongoose';
 import { ENUM_LOGGER_ACTION, ENUM_LOGGER_LEVEL } from './logger.constant';
 import { LoggerEntity } from './logger.schema';
 import { LoggerDocument } from './logger.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class LoggerService {
+    private readonly testMode: boolean;
+
     constructor(
         @InjectModel(LoggerEntity.name)
-        private readonly loggerModel: Model<LoggerDocument>
-    ) {}
+        private readonly loggerModel: Model<LoggerDocument>,
+        private readonly configService: ConfigService
+    ) {
+        this.testMode = this.configService.get<string>('app.env') !== 'testing';
+    }
 
     async info(
         action: ENUM_LOGGER_ACTION,
@@ -18,6 +24,10 @@ export class LoggerService {
         user?: string,
         tags?: string[]
     ): Promise<LoggerDocument> {
+        if (this.testMode) {
+            return;
+        }
+
         return this.loggerModel.create({
             level: ENUM_LOGGER_LEVEL.INFO,
             user: new Types.ObjectId(user),
@@ -34,6 +44,10 @@ export class LoggerService {
         user?: string,
         tags?: string[]
     ): Promise<LoggerDocument> {
+        if (this.testMode) {
+            return;
+        }
+
         return this.loggerModel.create({
             level: ENUM_LOGGER_LEVEL.DEBUG,
             user: new Types.ObjectId(user),
@@ -50,6 +64,10 @@ export class LoggerService {
         user?: string,
         tags?: string[]
     ): Promise<LoggerDocument> {
+        if (this.testMode) {
+            return;
+        }
+
         return this.loggerModel.create({
             level: ENUM_LOGGER_LEVEL.WARM,
             user: new Types.ObjectId(user),
@@ -66,6 +84,10 @@ export class LoggerService {
         user?: string,
         tags?: string[]
     ): Promise<LoggerDocument> {
+        if (this.testMode) {
+            return;
+        }
+
         return this.loggerModel.create({
             level: ENUM_LOGGER_LEVEL.FATAL,
             user: new Types.ObjectId(user),
