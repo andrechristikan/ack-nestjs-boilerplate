16a7d2907ea221a86ce2a056f3b15540d13540cf
andrechristikan <andrechristikan@gmail.com>
2022-01-08 14:29:05 +0700

    kafka request validation issue, cant validate

--- src/kafka/request/kafka.request.decorator.ts
+++ src/kafka/request/kafka.request.decorator.ts
@@ -1,8 +1,21 @@
-import { applyDecorators, UseFilters } from '@nestjs/common';
+import { applyDecorators, UseFilters, UsePipes } from '@nestjs/common';
 import { MessagePattern, Transport } from '@nestjs/microservices';
+import { ClassConstructor } from 'class-transformer';
 import { ErrorRcpFilter } from '../error/kafka.error.filter';
+import { RequestKafkaValidationPipe } from './pipe/request.kafka.validation.pipe';
 
-export function KafkaRequest(topic: string): any {
+export function KafkaRequest(
+    topic: string,
+    classValidation?: ClassConstructor<any>
+): any {
+    if (classValidation) {
+        return applyDecorators(
+            MessagePattern(topic, Transport.KAFKA),
+            UseFilters(ErrorRcpFilter),
+            UsePipes(RequestKafkaValidationPipe(classValidation))
+        );
+    }
+
     return applyDecorators(
         MessagePattern(topic, Transport.KAFKA),
         UseFilters(ErrorRcpFilter)
--- src/kafka/request/pipe/request.kafka.validation.pipe.ts
+++ src/kafka/request/pipe/request.kafka.validation.pipe.ts
@@ -1,56 +1,59 @@
-import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, Type, mixin } from '@nestjs/common';
 import { validate } from 'class-validator';
 import { Debugger } from 'src/debugger/debugger.decorator';
 import { Logger as DebuggerService } from 'winston';
-import { plainToInstance } from 'class-transformer';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { RpcException } from '@nestjs/microservices';
 import { ENUM_KAFKA_REQUEST_STATUS_CODE_ERROR } from '../kafka.request.constant';
 
-export class RequestKafkaValidationPipe implements PipeTransform {
-    constructor(
-        @Debugger() private readonly debuggerService: DebuggerService
-    ) {}
+export function RequestKafkaValidationPipe(
+    classValidation: ClassConstructor<any>
+): Type<PipeTransform> {
+    class MixinRequestKafkaValidationPipe implements PipeTransform {
+        constructor(
+            @Debugger() private readonly debuggerService: DebuggerService
+        ) {}
 
-    async transform(
-        value: Record<string, any>,
-        { metatype }: ArgumentMetadata
-    ): Promise<Record<string, any>> {
-        const metatypeObject = metatype as Record<string, any>;
-        const validation = metatypeObject.value;
-        if (!validation || !this.toValidate(validation)) {
+        async transform(
+            value: Record<string, any>
+        ): Promise<Record<string, any>> {
+            if (!classValidation || !this.toValidate(classValidation)) {
+                return value;
+            }
+
+            const request: Record<string, any> = plainToInstance(
+                classValidation,
+                value.value
+            );
+            this.debuggerService.info('Request Kafka Data', {
+                class: 'RequestKafkaValidationPipe',
+                function: 'transform',
+                request: request
+            });
+
+            const rawErrors: Record<string, any>[] = await validate(request);
+            if (rawErrors.length > 0) {
+                this.debuggerService.error('Request Kafka Errors', {
+                    class: 'RequestKafkaValidationPipe',
+                    function: 'transform',
+                    errors: rawErrors
+                });
+
+                throw new RpcException({
+                    statusCode:
+                        ENUM_KAFKA_REQUEST_STATUS_CODE_ERROR.KAFKA_REQUEST_VALIDATION_ERROR,
+                    message: 'http.clientError.unprocessableEntity',
+                    errors: rawErrors
+                });
+            }
             return value;
         }
 
-        const request: Record<string, any> = plainToInstance(
-            validation,
-            value.value
-        );
-        this.debuggerService.info('Request Kafka Data', {
-            class: 'RequestKafkaValidationPipe',
-            function: 'transform',
-            request: request
-        });
-
-        const rawErrors: Record<string, any>[] = await validate(request);
-        if (rawErrors.length > 0) {
-            this.debuggerService.error('Request Kafka Errors', {
-                class: 'RequestKafkaValidationPipe',
-                function: 'transform',
-                errors: rawErrors
-            });
-
-            throw new RpcException({
-                statusCode:
-                    ENUM_KAFKA_REQUEST_STATUS_CODE_ERROR.KAFKA_REQUEST_VALIDATION_ERROR,
-                message: 'http.clientError.unprocessableEntity',
-                errors: rawErrors
-            });
+        private toValidate(metatype: Record<string, any>): boolean {
+            const types: Record<string, any>[] = [];
+            return types.includes(metatype);
         }
-        return value;
     }
 
-    private toValidate(metatype: Record<string, any>): boolean {
-        const types: Record<string, any>[] = [];
-        return types.includes(metatype);
-    }
+    return mixin(MixinRequestKafkaValidationPipe);
 }
