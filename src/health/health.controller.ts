af60840daed42aa0a9213b2b4650048f3eca4785
andrechristikan <andrechristikan@gmail.com>
2022-03-12 16:33:59 +0700

    e2e, cover response decorator for health check endpoint

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -8,13 +8,20 @@ import {
 } from '@nestjs/terminus';
 import { Connection } from 'mongoose';
 import { DatabaseConnection } from 'src/database/database.decorator';
+import { Response } from 'src/response/response.decorator';
+import { IResponse } from 'src/response/response.interface';
 import { AwsHealthIndicator } from './indicator/health.aws.indicator';
+import { Logger as DebuggerService } from 'winston';
+import { Debugger } from 'src/debugger/debugger.decorator';
+import { ENUM_HEALTH_STATUS_CODE_ERROR } from './health.contant';
+import { SuccessException } from 'src/error/error.exception';
 @Controller({
     version: VERSION_NEUTRAL,
     path: 'health',
 })
 export class HealthController {
     constructor(
+        @Debugger() private readonly debuggerService: DebuggerService,
         @DatabaseConnection() private readonly databaseConnection: Connection,
         private readonly health: HealthCheckService,
         private readonly memoryHealthIndicator: MemoryHealthIndicator,
@@ -23,33 +30,51 @@ export class HealthController {
         private readonly awsIndicator: AwsHealthIndicator
     ) {}
 
-    @Get('/')
+    @Response('health.check')
     @HealthCheck()
-    check() {
-        return this.health.check([
-            () => this.awsIndicator.isHealthy('aws bucket'),
-            // the process should not use more than 300MB memory
-            () =>
-                this.memoryHealthIndicator.checkHeap(
-                    'memory heap',
-                    300 * 1024 * 1024
-                ),
-            // The process should not have more than 300MB RSS memory allocated
-            () =>
-                this.memoryHealthIndicator.checkRSS(
-                    'memory RSS',
-                    300 * 1024 * 1024
-                ),
-            // the used disk storage should not exceed the 75% of the available space
-            () =>
-                this.diskHealthIndicator.checkStorage('disk health', {
-                    thresholdPercent: 0.75,
-                    path: '/',
-                }),
-            () =>
-                this.databaseIndicator.pingCheck('database', {
-                    connection: this.databaseConnection,
-                }),
-        ]);
+    @Get('/')
+    async check(): Promise<IResponse> {
+        try {
+            const health = await this.health.check([
+                () => this.awsIndicator.isHealthy('aws bucket'),
+                // the process should not use more than 300MB memory
+                () =>
+                    this.memoryHealthIndicator.checkHeap(
+                        'memory heap',
+                        300 * 1024 * 1024
+                    ),
+                // The process should not have more than 300MB RSS memory allocated
+                () =>
+                    this.memoryHealthIndicator.checkRSS(
+                        'memory RSS',
+                        300 * 1024 * 1024
+                    ),
+                // the used disk storage should not exceed the 75% of the available space
+                () =>
+                    this.diskHealthIndicator.checkStorage('disk health', {
+                        thresholdPercent: 0.75,
+                        path: '/',
+                    }),
+                () =>
+                    this.databaseIndicator.pingCheck('database', {
+                        connection: this.databaseConnection,
+                    }),
+            ]);
+
+            return health;
+        } catch (err) {
+            this.debuggerService.error('Health try catch', {
+                class: 'HealthController',
+                function: 'check',
+                error: err,
+            });
+
+            throw new SuccessException({
+                statusCode:
+                    ENUM_HEALTH_STATUS_CODE_ERROR.HEALTH_UNHEALTHY_ERROR,
+                message: 'health.error.check',
+                data: err.response,
+            });
+        }
     }
 }
