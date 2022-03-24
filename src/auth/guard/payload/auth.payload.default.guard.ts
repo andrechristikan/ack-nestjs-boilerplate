e441e0b9c648352c867db2e7e24070aa88bea531
andrechristikan <andrechristikan@gmail.com>
2022-03-25 01:27:18 +0700

    status code update spesific

--- src/auth/guard/payload/auth.payload.default.guard.ts
+++ src/auth/guard/payload/auth.payload.default.guard.ts
@@ -14,7 +14,7 @@ export class AuthPayloadDefaultGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const { user } = context.switchToHttp().getRequest();
 
-        if (!user.isActive || !user.role.isActive) {
+        if (!user.isActive) {
             this.debuggerService.error(
                 'UserGuard Inactive',
                 'AuthDefaultGuard',
@@ -23,8 +23,20 @@ export class AuthPayloadDefaultGuard implements CanActivate {
 
             throw new ForbiddenException({
                 statusCode:
-                    ENUM_AUTH_STATUS_CODE_ERROR.AUTH_GUARD_JWT_ACCESS_TOKEN_ERROR,
+                    ENUM_AUTH_STATUS_CODE_ERROR.AUTH_GUARD_INACTIVE_ERROR,
                 message: 'auth.error.blocked',
+            });
+        } else if (!user.role.isActive) {
+            this.debuggerService.error(
+                'UserGuard Role Inactive',
+                'AuthDefaultGuard',
+                'canActivate'
+            );
+
+            throw new ForbiddenException({
+                statusCode:
+                    ENUM_AUTH_STATUS_CODE_ERROR.AUTH_GUARD_ROLE_INACTIVE_ERROR,
+                message: 'auth.error.roleBlocked',
             });
         }
 
