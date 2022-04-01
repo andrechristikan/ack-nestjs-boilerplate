3528bcd6853c4f2341ec5267d3be0562173f0705
andrechristikan <andrechristikan@gmail.com>
2022-04-02 01:52:58 +0700

    upddate package version

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,6 +1,6 @@
 import { Module, Global } from '@nestjs/common';
 import * as path from 'path';
-import { I18nModule, I18nJsonParser, HeaderResolver } from 'nestjs-i18n';
+import { I18nModule, HeaderResolver, I18nJsonLoader } from 'nestjs-i18n';
 import { ConfigService } from '@nestjs/config';
 import { ENUM_MESSAGE_LANGUAGE } from './message.constant';
 import { MessageService } from './service/message.service';
@@ -17,12 +17,12 @@ import { MessageService } from './service/message.service';
                     (a, v) => ({ ...a, [`${v}-*`]: v }),
                     {}
                 ),
-                parserOptions: {
+                loaderOptions: {
                     path: path.join(__dirname, '/languages/'),
                     watch: true,
                 },
             }),
-            parser: I18nJsonParser,
+            loader: I18nJsonLoader,
             inject: [ConfigService],
             resolvers: [new HeaderResolver(['x-custom-lang'])],
         }),
