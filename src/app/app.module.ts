e43d63746816b3be0f7432513cf4eddb522981fb
Andre Christi Kan <andrechristikan@gmail.com>
2022-04-20 12:35:27 +0700

    router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
 import { CoreModule } from 'src/core/core.module';
-import { RouterAdminModule } from 'src/router/router.admin.module';
-import { RouterCommonModule } from 'src/router/router.common.module';
-import { RouterEnumModule } from 'src/router/router.enum.module';
-import { RouterPublicModule } from 'src/router/router.public.module';
-import { RouterTestModule } from 'src/router/router.test.module';
+import { AppRouterModule } from './app.router.module';
 @Module({
     controllers: [],
     providers: [],
@@ -14,33 +9,7 @@ import { RouterTestModule } from 'src/router/router.test.module';
         CoreModule,
 
         // Router
-        RouterCommonModule,
-        RouterTestModule,
-        RouterEnumModule,
-        RouterPublicModule,
-        RouterAdminModule,
-        RouterModule.register([
-            {
-                path: '/',
-                module: RouterCommonModule,
-            },
-            {
-                path: '/test',
-                module: RouterTestModule,
-            },
-            {
-                path: '/enum',
-                module: RouterEnumModule,
-            },
-            {
-                path: '/admin',
-                module: RouterAdminModule,
-            },
-            {
-                path: '/public',
-                module: RouterPublicModule,
-            },
-        ]),
+        AppRouterModule,
     ],
 })
 export class AppModule {}
