f65ccbca23e55d066861f62ae4547f1ff6b63191
Andre Christi Kan <andrechristikan@gmail.com>
2022-05-10 15:56:17 +0700

    Update auth.api.list.serialization.ts

--- src/auth/serialization/auth.api.list.serialization.ts
+++ src/auth/serialization/auth.api.list.serialization.ts
@@ -5,17 +5,7 @@ export class AuthApiListSerialization {
     readonly _id: string;
 
     readonly name: string;
-
-    @Exclude()
-    readonly description?: string;
     readonly key: string;
-
-    @Exclude()
-    readonly hash: string;
-
     readonly isActive: boolean;
     readonly createdAt: Date;
-
-    @Exclude()
-    readonly updatedAt: Date;
 }
