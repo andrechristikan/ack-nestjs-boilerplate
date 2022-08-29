1c3c8552ac76d62ddd3c275a89371a55a1e0fa64
Andre Christi Kan <andrechristikan@gmail.com>
2022-08-29 23:29:39 +0700

    add database interface options

--- src/common/database/database.interface.ts
+++ src/common/database/database.interface.ts
@@ -1,9 +1,13 @@
+import { ClientSession } from 'mongoose';
 import { IPaginationOptions } from '../pagination/pagination.interface';
 
 export interface IDatabaseFindOneOptions {
     populate?: Record<string, boolean>;
+    session?: ClientSession;
 }
 
 export interface IDatabaseFindAllOptions
     extends IPaginationOptions,
         IDatabaseFindOneOptions {}
+
+export type IDatabaseOptions = Pick<IDatabaseFindOneOptions, 'session'>;
