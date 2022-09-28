aabc02d83886e37b608f1dcd2a7a3d75c3e798b0
Andre Christi Kan <andrechristikan@gmail.com>
2022-09-28 23:44:26 +0700

    Update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     try {
         await app.select(CommandModule).get(CommandService).exec();
         await app.close();
+        process.exit(0);
     } catch (err: any) {
         logger.error(err, 'Migration');
         await app.close();
