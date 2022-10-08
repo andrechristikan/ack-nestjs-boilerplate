1da5b15a1fd88ea41970e168551623f8ca4ccb3a
Andre Christi Kan <andrechristikan@gmail.com>
2022-10-08 20:55:30 +0700

    up

--- src/modules/user/serializations/user.payload.serialization.ts
+++ src/modules/user/serializations/user.payload.serialization.ts
@@ -1,5 +1,8 @@
-import { OmitType } from '@nestjs/swagger';
+import { faker } from '@faker-js/faker';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { Exclude, Transform } from 'class-transformer';
+import { ENUM_AUTH_ACCESS_FOR } from 'src/common/auth/constants/auth.enum.constant';
+import { ENUM_AUTH_PERMISSIONS } from 'src/common/auth/constants/auth.enum.permission.constant';
 import { AwsS3Serialization } from 'src/common/aws/serializations/aws.s3.serialization';
 import { IUserRolePayload } from 'src/modules/user/interfaces/user.interface';
 import { UserGetSerialization } from './user.get.serialization';
@@ -8,10 +11,19 @@ export class UserPayloadSerialization extends OmitType(UserGetSerialization, [
     'photo',
     'role',
     'isActive',
+    'passwordExpired',
 ] as const) {
     @Exclude()
     readonly photo?: AwsS3Serialization;
 
+    @ApiProperty({
+        example: {
+            name: faker.name.jobTitle(),
+            permissions: Object.values(ENUM_AUTH_PERMISSIONS),
+            accessFor: ENUM_AUTH_ACCESS_FOR.ADMIN,
+        },
+        type: 'object',
+    })
     @Transform(({ value }) => ({
         name: value.name,
         permissions: value.permissions.map(
@@ -23,4 +35,7 @@ export class UserPayloadSerialization extends OmitType(UserGetSerialization, [
 
     @Exclude()
     readonly isActive: boolean;
+
+    @Exclude()
+    readonly passwordExpired: Date;
 }
