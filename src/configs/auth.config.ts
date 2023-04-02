13cf4fb6abab0e70a6af4cba288b764c57bb93f1
ack <andrechristikan@gmail.com>
2023-04-02 21:57:20 +0700

    setting update

--- src/configs/auth.config.ts
+++ src/configs/auth.config.ts
@@ -7,9 +7,9 @@ export default registerAs(
         accessToken: {
             secretKey: process.env.AUTH_JWT_ACCESS_TOKEN_SECRET_KEY ?? '123456',
             expirationTime: seconds(
-                process.env.AUTH_JWT_ACCESS_TOKEN_EXPIRED ?? '15m'
-            ), // recommendation for production is 15m
-            notBeforeExpirationTime: seconds('0'), // keep it in zero value
+                process.env.AUTH_JWT_ACCESS_TOKEN_EXPIRED ?? '1h'
+            ), // 1 hours
+            notBeforeExpirationTime: seconds('0'), // immediately
 
             encryptKey: process.env.AUTH_JWT_PAYLOAD_ACCESS_TOKEN_ENCRYPT_KEY,
             encryptIv: process.env.AUTH_JWT_PAYLOAD_ACCESS_TOKEN_ENCRYPT_IV,
@@ -19,12 +19,11 @@ export default registerAs(
             secretKey:
                 process.env.AUTH_JWT_REFRESH_TOKEN_SECRET_KEY ?? '123456000',
             expirationTime: seconds(
-                process.env.AUTH_JWT_REFRESH_TOKEN_EXPIRED ?? '7d'
-            ), // recommendation for production is 7d
+                process.env.AUTH_JWT_REFRESH_TOKEN_EXPIRED ?? '14d'
+            ), // 14 days
             notBeforeExpirationTime: seconds(
-                process.env.AUTH_JWT_REFRESH_TOKEN_NOT_BEFORE_EXPIRATION ??
-                    '15m'
-            ), // recommendation for production is 15m
+                process.env.AUTH_JWT_REFRESH_TOKEN_NOT_BEFORE_EXPIRATION ?? '1h'
+            ), // 1 hours
 
             encryptKey: process.env.AUTH_JWT_PAYLOAD_REFRESH_TOKEN_ENCRYPT_KEY,
             encryptIv: process.env.AUTH_JWT_PAYLOAD_REFRESH_TOKEN_ENCRYPT_IV,
@@ -39,9 +38,9 @@ export default registerAs(
 
         password: {
             attempt: true,
-            maxAttempt: 3,
-            saltLength: 8,
-            expiredIn: seconds('182d'), // recommendation for production is 182 days
+            maxAttempt: 5,
+            saltLength: 16,
+            expiredIn: seconds('182d'), // 182 days
         },
     })
 );
