76ec1dc2f780195bb55cbc5396a51b7799b18975
ack <andrechristikan@gmail.com>
2023-04-08 17:01:02 +0700

    add spesific inactive permanent

--- src/modules/user/constants/user.constant.ts
+++ src/modules/user/constants/user.constant.ts
@@ -1,2 +1,3 @@
 export const USER_ACTIVE_META_KEY = 'UserActiveMetaKey';
+export const USER_INACTIVE_PERMANENT_META_KEY = 'UserInactivePermanentMetaKey';
 export const USER_BLOCKED_META_KEY = 'UserBlockedMetaKey';
