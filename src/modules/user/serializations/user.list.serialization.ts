afc75ee13cd9e1f029371b7429fcba32e46195c7
ack <andrechristikan@gmail.com>
2023-04-23 23:05:56 +0700

    policy guard update

--- src/modules/user/serializations/user.list.serialization.ts
+++ src/modules/user/serializations/user.list.serialization.ts
@@ -1,13 +1,18 @@
 import { OmitType } from '@nestjs/swagger';
-import { Exclude } from 'class-transformer';
+import { Exclude, Type } from 'class-transformer';
 import { AwsS3Serialization } from 'src/common/aws/serializations/aws.s3.serialization';
+import { RoleListSerialization } from 'src/modules/role/serializations/role.list.serialization';
 import { UserProfileSerialization } from 'src/modules/user/serializations/user.profile.serialization';
 
 export class UserListSerialization extends OmitType(UserProfileSerialization, [
     'photo',
     'signUpDate',
     'signUpFrom',
+    'role',
 ] as const) {
+    @Type(() => RoleListSerialization)
+    readonly role: RoleListSerialization;
+
     @Exclude()
     readonly photo?: AwsS3Serialization;
 
