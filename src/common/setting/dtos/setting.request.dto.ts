e2e7afa2378dceb132f1e55e46445b586c42e4d2
ack <andrechristikan@gmail.com>
2023-06-18 00:47:21 +0700

    remove request dto swagger api property because that is not nessasary

--- src/common/setting/dtos/setting.request.dto.ts
+++ src/common/setting/dtos/setting.request.dto.ts
@@ -1,14 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class SettingRequestDto {
-    @ApiProperty({
-        name: 'setting',
-        description: 'setting id',
-        required: true,
-        nullable: false,
-    })
     @IsNotEmpty()
     @IsUUID('4')
     @Type(() => String)
--- src/modules/role/dtos/role.request.dto.ts
+++ src/modules/role/dtos/role.request.dto.ts
@@ -1,14 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class RoleRequestDto {
-    @ApiProperty({
-        name: 'role',
-        description: 'role id',
-        required: true,
-        nullable: false,
-    })
     @IsNotEmpty()
     @IsUUID('4')
     @Type(() => String)
