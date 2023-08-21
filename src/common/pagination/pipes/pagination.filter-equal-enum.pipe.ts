5b9fbd23f22d2455df8c3605216fa9324648d5c8
ack <andrechristikan@gmail.com>
2023-08-21 17:04:39 +0700

    add body example to /role/create endpoint, add pagination filter equal enum

---
+++ src/common/pagination/pipes/pagination.filter-equal-enum.pipe.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable, mixin, Type } from '@nestjs/common';
+import { PipeTransform, Scope } from '@nestjs/common/interfaces';
+import { REQUEST } from '@nestjs/core';
+import { PaginationService } from 'src/common/pagination/services/pagination.service';
+import { IRequestApp } from 'src/common/request/interfaces/request.interface';
+
+export function PaginationFilterEqualEnumPipe<T>(
+    field: string,
+    defaultValue: T,
+    defaultEnum: Record<string, any>,
+    raw: boolean
+): Type<PipeTransform> {
+    @Injectable({ scope: Scope.REQUEST })
+    class MixinPaginationFilterInEnumPipe implements PipeTransform {
+        constructor(
+            @Inject(REQUEST) protected readonly request: IRequestApp,
+            private readonly paginationService: PaginationService
+        ) {}
+
+        async transform(value: string): Promise<Record<string, T>> {
+            const finalValue: T = value
+                ? defaultEnum[value.toUpperCase()] ?? defaultValue
+                : defaultValue;
+
+            if (raw) {
+                return {
+                    [field]: finalValue,
+                };
+            }
+
+            return this.paginationService.filterEqual<T>(field, finalValue);
+        }
+    }
+
+    return mixin(MixinPaginationFilterInEnumPipe);
+}
