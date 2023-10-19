dffefc6c3455008e24481c4afcd2d8009c1161b7
ack <andrechristikan@gmail.com>
2023-10-19 15:30:45 +0700

    unsed variable

--- src/modules/role/constants/role.constant.ts
+++ src/modules/role/constants/role.constant.ts
@@ -1,5 +1,2 @@
 export const ROLE_IS_ACTIVE_META_KEY = 'RoleIsActiveMetaKey';
 export const ROLE_TYPE_META_KEY = 'RoleTypeMetaKey';
-
-export const ROLE_LAWYER_NAME = 'lawyer';
-export const ROLE_CLIENT_NAME = 'client';
