d059d788a7257044666503feb02370a5844eddd5
ack <andrechristikan@gmail.com>
2024-02-06 13:58:12 +0700

    dashboard serialization total and percent

--- src/modules/dashboard/serializations/dashboard.serialization.ts
+++ src/modules/dashboard/serializations/dashboard.serialization.ts
@@ -2,14 +2,42 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class DashboardSerialization {
     @ApiProperty({
-        name: 'name',
-        nullable: true,
+        name: 'startDate',
+        required: true,
     })
     startDate: Date;
 
     @ApiProperty({
-        name: 'month',
-        nullable: true,
+        name: 'endDate',
+        required: true,
     })
     endDate: Date;
+}
+
+export class DashboardTotalSerialization {
+    @ApiProperty({
+        name: 'total',
+        required: true,
+    })
+    total: number;
+
+    @ApiProperty({
+        name: 'percent',
+        required: false,
+    })
+    percent?: number;
+}
+
+export class DashboardTotalByMonthSerialization extends DashboardTotalSerialization {
+    @ApiProperty({
+        name: 'month',
+        required: true,
+    })
+    month: number;
+
+    @ApiProperty({
+        name: 'year',
+        required: true,
+    })
+    year: number;
 }
