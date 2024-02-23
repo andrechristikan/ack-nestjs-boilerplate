623088f975a33458a79b2cb548c3d910ed6ecd52
ack <andrechristikan@gmail.com>
2024-02-23 09:50:37 +0700

    fix env config

--- src/modules/dashboard/constants/dashboard.doc.constant.ts
+++ src/modules/dashboard/constants/dashboard.doc.constant.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+import { ENUM_DASHBOARD_DAY } from 'src/modules/dashboard/constants/dashboard.enum.constant';
 
 export const DashboardDocQueryDate = [
     {
@@ -14,5 +15,17 @@ export const DashboardDocQueryDate = [
         required: false,
         type: 'string',
         example: faker.date.recent(),
+    },
+];
+
+export const DashboardDocQueryDay = [
+    {
+        name: 'day',
+        allowEmptyValue: true,
+        required: false,
+        type: 'string',
+        example: ENUM_DASHBOARD_DAY.LAST_WEEK,
+        enum: ENUM_DASHBOARD_DAY,
+        description: "value with ',' delimiter",
     },
 ];
