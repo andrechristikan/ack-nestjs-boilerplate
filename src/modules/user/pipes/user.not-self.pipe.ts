108db2f6da7fb90e300713a8fcbf9636dee1583e
andrechristikan <andrechristikan@gmail.com>
2024-06-20 17:37:57 +0700

    fix: user not self pipe

--- src/modules/user/pipes/user.not-self.pipe.ts
+++ src/modules/user/pipes/user.not-self.pipe.ts
@@ -6,6 +6,7 @@ import {
     Scope,
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
+import { ENUM_POLICY_ROLE_TYPE } from 'src/common/policy/constants/policy.enum.constant';
 import { IRequestApp } from 'src/common/request/interfaces/request.interface';
 import { ENUM_USER_STATUS_CODE_ERROR } from 'src/modules/user/constants/user.status-code.constant';
 
@@ -15,7 +16,10 @@ export class UserNotSelfPipe implements PipeTransform {
 
     async transform(value: string): Promise<string> {
         const { user } = this.request;
-        if (user._id === value && user.type) {
+        if (
+            user._id === value &&
+            user.type !== ENUM_POLICY_ROLE_TYPE.SUPER_ADMIN
+        ) {
             throw new NotFoundException({
                 statusCode: ENUM_USER_STATUS_CODE_ERROR.NOT_FOUND_ERROR,
                 message: 'user.error.notFound',
