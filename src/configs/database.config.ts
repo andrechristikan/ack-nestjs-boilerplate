484bba6f62557dcb7e9a7fd25aaab0295bbe3d50
andrechristikan <andrechristikan@gmail.com>
2024-11-10 23:38:43 +0700

    fix: database URL env

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -4,7 +4,7 @@ export default registerAs(
     'database',
     (): Record<string, any> => ({
         url:
-            process.env?.DATABASE_URI ??
+            process.env?.DATABASE_URL ??
             'mongodb://localhost:27017,localhost:27018,localhost:27019',
 
         debug: process.env.DATABASE_DEBUG === 'true',
