8e7ffd8d7c4e25baab5361b54532014eba91cc8e
andrechristikan <andrechristikan@gmail.com>
2025-06-26 23:02:53 +0700

    fix: env example update, and worker base for error handling

--- src/modules/sms/processors/sms.processor.ts
+++ src/modules/sms/processors/sms.processor.ts
@@ -1,16 +1,17 @@
 import { ENUM_SEND_SMS_PROCESS } from '@modules/sms/enums/sms.enum';
-import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Processor } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { SmsSendRequestDto } from '@modules/sms/dtos/request/sms.send.request.dto';
 import { SmsVerificationRequestDto } from '@modules/sms/dtos/request/sms.verification.request.dto';
 import { ISmsProcessor } from '@modules/sms/interfaces/sms.processor.interface';
 import { SmsService } from '@modules/sms/services/sms.service';
 import { ENUM_WORKER_QUEUES } from '@workers/enums/worker.enum';
+import { WorkerBase } from '@workers/bases/worker.base';
 
 @Processor({
     name: ENUM_WORKER_QUEUES.SMS_QUEUE,
 })
-export class SmsProcessor extends WorkerHost implements ISmsProcessor {
+export class SmsProcessor extends WorkerBase implements ISmsProcessor {
     constructor(private readonly smsService: SmsService) {
         super();
     }
